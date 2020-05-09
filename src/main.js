e8c6ca6ff29ee996a7147c321df6a171ac6f6f93
cheng-long <1760016317@qq.com>
2020-05-09 09:58:20 +0800

    调整目录初始化

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// import '@/style/index.less'
 Vue.config.productionTip = false
 
+// 创建vue根实例
 new Vue({
   router,
   store,
